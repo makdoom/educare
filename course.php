939575cc9aa885de5cb3707cf33e8e64ab429649
Makdoom <makshaikh99@gmail.com>
2020-02-19 20:58:47 +0530

    working in course section responsiveness

--- course.php
+++ course.php
@@ -6,27 +6,44 @@
                 <h1>courses</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ducimus ex ea quaerat eos accusantium neque quas temporibus quis, quo voluptas! Cum temporibus earum expedita, ipsam vero cupiditate aut eligendi soluta reiciendis dicta perspiciatis fugiat, consectetur totam omnis, autem quaerat.</p>
             </div>
-            <div class="admission">
+            
+        </div>
+        <div class="admission">
                 <h3>Admission</h3>
                 <div class="inputs">
-                    <input type="text" placeholder="Full Name">
-                    <input type="text" placeholder="Email">
-                    <input type="text" placeholder="Location">
-                    <select name="Choose Course" id="" required>
-                        <option value="course">Select Course</option>
-                        <option value="secondary">secondary</option>
-                        <option value="higher secondary">higher secondary </option>
-                        <option value="pharmacy">pharmacy</option>
-                    </select>
-                    <textarea name="" id="" placeholder="Message"></textarea>
-                    <input type="submit" value="Submit">
+                    <form >
+                        <input type="text" placeholder="Full Name" required="required"> 
+                        <input type="email" placeholder="Email"required="required">
+                        <input type="text" placeholder="Location"required="required">
+                        <select name="Choose Course" id="" required="required">
+                            <option value="course">Select Course</option>
+                            <option value="secondary">secondary</option>
+                            <option value="higher secondary">higher secondary </option>
+                            <option value="pharmacy">pharmacy</option>
+                        </select>
+                        <textarea name="" id="" placeholder="Message"></textarea>
+                        <input type="submit" value="Submit" id="submitbtn">
+                    </form>
                 </div>
             </div>
-        </div>
+            <div class="courses">
+                <div class="box">
+                    <h3>Secondary</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores commodi esse reprehenderit molestiae voluptatum cupiditate aliquam ducimus! Dolore recusandae ex eum excepturi aut eius odio harum dolorem </p>
+                    <a href="#">Read More</a>
+                </div>
+                <div class="box">
+                    <h3>Higher Secondary</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores commodi esse reprehenderit molestiae voluptatum cupiditate aliquam ducimus! Dolore recusandae ex eum excepturi aut eius odio harum dolorem </p>
+                    <a href="#">Read More</a>
+                </div>
+                <div class="box">
+                    <h3>Pharmacy</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores commodi esse reprehenderit molestiae voluptatum cupiditate aliquam ducimus! Dolore recusandae ex eum excepturi aut eius odio harum dolorem </p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
         <div class="slantdiv"></div>
-        <div class="test">
-            <h2>Different Courses</h2>
-        </div>
     </div>
 
 </div>
